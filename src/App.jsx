e8d390ec8c9b31eba8e98bdb77b705a6b508e81c
gracebir <grace.devolop@gmail.com>
2023-06-25 13:30:12 +0200

    other components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Mockup from './components/Mockup'
+import Illustractions from './components/Illustractions'
+import Action from './components/Action'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Navbar/>
+      <main className='container mx-auto'>
+        <Hero/>
+        <Mockup/>
+        <Illustractions/>
+        <Action/>
+      </main>
+      <Footer/>
+    </>
   )
 }
 
---
+++ src/components/Illustractions.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Illustractions() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Illustractions
