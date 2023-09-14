3ea744b6b6866076ca1d293c32061668b5e73ad1
gracebir <grace.devolop@gmail.com>
2023-09-14 19:45:21 +0200

    import export asserts

---
+++ src/assets/index.js
@@ -0,0 +1,9 @@
+import growth from '../assets/illustration-grow-together.svg'
+import flowing from '../assets/illustration-flowing-conversation.svg'
+import yourUser from '../assets/illustration-your-users.svg'
+
+export {
+    growth,
+    flowing,
+    yourUser
+}
