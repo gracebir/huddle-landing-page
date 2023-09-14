ec730f6c93d88152bcd6ef29e48d4e1c022348ec
gracebir <grace.devolop@gmail.com>
2023-09-14 21:21:07 +0200

    action section

--- src/sections/Action.jsx
+++ src/sections/Action.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 function Action() {
   return (
-    <div>
-      
-    </div>
+    <section className='lg:h-[70vh] h-[50vh] w-full flex items-center'>
+      <div className='flex flex-col items-center gap-10 lg:gap-14 max-w-5xl mx-auto font-bold text-center'>
+        <h1 className="font-poppins text-2xl lg:text-5xl">Ready To Build Your Community?</h1>
+        <a href="#" className='py-3 text-white lg:py-6 px-6 lg:px-36 lg:text-xl text-base bg-color-pink hover:bg-color-light-pink shadow-lg rounded-full'>
+          Get Started For Free
+        </a>
+      </div>
+    </section>
   )
 }
 
